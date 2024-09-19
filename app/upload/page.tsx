6a5d682219a33e85ae82f1657c591653a6b5c059
Ahdaaa <afilzag@gmail.com>
2024-09-19 19:50:35 +0700

    style: upload section

--- app/upload/page.tsx
+++ app/upload/page.tsx
@@ -1,7 +1,7 @@
 import UploadFile from "@/components/uploadFile/uploadFile";
 const uploadPage = () => {
   return (
-    <section className="my-20 mt-28">
+    <section className="my-20 mt-28 flex justify-center">
       <UploadFile />
     </section>
   );
