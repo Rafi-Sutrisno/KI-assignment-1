bf198f2e8d872dbb8565a04901d740e4448f0802
Ahdaaa <afilzag@gmail.com>
2024-09-19 19:45:55 +0700

    feat: link to login page

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <nav className="fixed w-full z-20 top-0 start-0 border-b bg-white">
+    <nav className="fixed w-full z-20 top-0 start-0 border-b bg-white dark:bg-black">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <a href="/" className="flex items-center space-x-3 rtl:space-x-reverse">
           <img src="/assets/bonfire.png" className="h-8" alt="Flowbite Logo" />
@@ -11,12 +11,12 @@ const Navbar = () => {
           </span>
         </a>
         <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          <button
-            type="button"
+          <Link
+            href={"/login"}
             className="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           >
             Get Started
-          </button>
+          </Link>
           <button
             data-collapse-toggle="navbar-sticky"
             type="button"
