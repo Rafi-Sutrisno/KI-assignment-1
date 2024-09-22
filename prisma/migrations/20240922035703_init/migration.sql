90f344e909002151faf4da6099901da99c401dee
Ahdaaa <afilzag@gmail.com>
2024-09-22 14:10:23 +0700

    init: prisma schema

---
+++ prisma/migrations/20240922035703_init/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phone_number" TEXT NOT NULL,
+    "address" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "UserFiles" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "fileType" TEXT NOT NULL,
+    "aes_encrypted" BYTEA NOT NULL,
+    "rc4_encrypted" BYTEA NOT NULL,
+    "des_encrypted" BYTEA NOT NULL,
+
+    CONSTRAINT "UserFiles_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- AddForeignKey
+ALTER TABLE "UserFiles" ADD CONSTRAINT "UserFiles_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+ALTER TABLE "User"
+ADD CONSTRAINT valid_phone_number CHECK (phone_number ~ '^(\+62|62|0)8[1-9][0-9]{6,10}$')
