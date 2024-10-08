00b3c30554a37316e2398615d2a5f1f9d34351ce
Rafi Sutrisno <mrafis2003@gmail.com>
2024-10-08 13:12:17 +0700

    fix hydration and login page authorization

--- components/Provider/TokenProvider.tsx
+++ components/Provider/TokenProvider.tsx
@@ -1,23 +1,24 @@
 "use client";
 
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
 import { ContextType } from "@/interface/token";
 import { ContextProviderProps } from "@/interface/token";
 
 export const Context = createContext<ContextType | undefined>(undefined);
 
 const TokenContextProvider: React.FC<ContextProviderProps> = (props) => {
-  // Initialize token state from localStorage only if it's available
-  const initialToken =
-    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+  const [token, setToken] = useState<string | null>(null);
+  const [isMounted, setIsMounted] = useState(false); // Track if component has mounted
 
-  const [token, setToken] = useState<string | null>(initialToken);
+  useEffect(() => {
+    const storedToken = localStorage.getItem("token");
+    setToken(storedToken); // Set the token from localStorage on client mount
+    setIsMounted(true); // Indicate that the component has mounted
+  }, []);
 
   const saveToken = (newToken: string) => {
     setToken(newToken);
-    if (typeof window !== "undefined") {
-      localStorage.setItem("token", newToken); // Save token to local storage
-    }
+    localStorage.setItem("token", newToken); // Save token to local storage
   };
 
   const getToken = () => {
@@ -25,11 +26,8 @@ const TokenContextProvider: React.FC<ContextProviderProps> = (props) => {
   };
 
   const resetToken = () => {
-    console.log("masuk resettoken");
     setToken(null);
-    if (typeof window !== "undefined") {
-      localStorage.removeItem("token"); // Remove token from local storage
-    }
+    localStorage.removeItem("token"); // Remove token from local storage
   };
 
   const contextValue = {
@@ -38,6 +36,11 @@ const TokenContextProvider: React.FC<ContextProviderProps> = (props) => {
     getToken,
     resetToken,
   };
+
+  // Render null until the component is mounted
+  if (!isMounted) {
+    return null; // Prevent hydration issues
+  }
 
   return (
     <Context.Provider value={contextValue}>{props.children}</Context.Provider>
