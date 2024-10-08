4bd914f556a2a85260ef0e3e23d52c7851711301
Rafi Sutrisno <mrafis2003@gmail.com>
2024-10-08 12:57:43 +0700

    fix reload token

--- components/Provider/TokenProvider.tsx
+++ components/Provider/TokenProvider.tsx
@@ -7,19 +7,34 @@ import { ContextProviderProps } from "@/interface/token";
 export const Context = createContext<ContextType | undefined>(undefined);
 
 const TokenContextProvider: React.FC<ContextProviderProps> = (props) => {
-  const [token, setToken] = useState<string | null>(null);
+  // Initialize token state from localStorage only if it's available
+  const initialToken =
+    typeof window !== "undefined" ? localStorage.getItem("token") : null;
+
+  const [token, setToken] = useState<string | null>(initialToken);
+
+  const saveToken = (newToken: string) => {
+    setToken(newToken);
+    if (typeof window !== "undefined") {
+      localStorage.setItem("token", newToken); // Save token to local storage
+    }
+  };
 
   const getToken = () => {
-    return token;
+    return token; // Return the current token state
   };
 
   const resetToken = () => {
+    console.log("masuk resettoken");
     setToken(null);
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("token"); // Remove token from local storage
+    }
   };
 
   const contextValue = {
     token,
-    setToken,
+    setToken: saveToken,
     getToken,
     resetToken,
   };
