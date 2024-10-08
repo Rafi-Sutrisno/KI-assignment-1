cc2bf5e75bfac17c2d701c338f2ff13d9044471e
Rafi Sutrisno <mrafis2003@gmail.com>
2024-10-08 13:18:35 +0700

    fix profile authorization

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,6 +1,24 @@
 import Profile from "@/components/profile/profile";
+import { Context } from "@/components/Provider/TokenProvider";
+import { useRouter } from "next/router";
+import { useContext, useEffect } from "react";
 
 const profilePage = () => {
+  const context = useContext(Context);
+
+  if (!context) {
+    throw new Error("Context must be used within a ContextProvider");
+  }
+
+  const { getToken } = context;
+  const router = useRouter();
+
+  useEffect(() => {
+    if (getToken()) {
+      router.push("/");
+    }
+  }, [getToken, router]);
+
   return (
     <section className="my-20 mt-28 flex justify-center">
       <Profile />
