dbb61e584cc024cac1bd8d6edb319cab3e18a7d6
Rafi Sutrisno <mrafis2003@gmail.com>
2024-10-14 18:04:56 +0700

    add time analysis for each algorithm

--- app/api/decryptDES/file/route.ts
+++ app/api/decryptDES/file/route.ts
@@ -1,8 +1,9 @@
 import { NextResponse } from "next/server";
-import { createDecipheriv} from "crypto";
+import { createDecipheriv } from "crypto";
 
 export async function POST(req: Request) {
   try {
+    console.time("DES Decryption Time");
     const { encryptedBuffer, ivDes } = await req.json(); // Extract encryptedInput from request body
     const encryptionKeyHex = process.env.NEXT_PUBLIC_ENCRYPTION_DES_KEY;
     const key = Buffer.from(encryptionKeyHex!, "hex");
@@ -22,6 +23,7 @@ export async function POST(req: Request) {
     decrypted = Buffer.concat([decrypted, decipher.final()]);
 
     console.log("Decrypted data: ", decrypted);
+    console.timeEnd("DES Decryption Time");
     return NextResponse.json({ decrypted });
   } catch (error) {
     console.error("Decryption error:", error);
--- components/encryptions/rc4.ts
+++ components/encryptions/rc4.ts
@@ -1,39 +1,21 @@
 import { createCipheriv, createDecipheriv } from "crypto";
 
 const algorithm = "rc4";
-const encryptionKeyHex = process.env.NEXT_PUBLIC_ENCRYPTION_DES_KEY;;
+const encryptionKeyHex = process.env.NEXT_PUBLIC_ENCRYPTION_DES_KEY;
 const key = Buffer.from(encryptionKeyHex!, "hex");
 
 export function encryptRC4(input: Buffer | string): Buffer | string {
   let cipher, encrypted;
+  console.time("RC4 Encryption Time");
   if (typeof input === "string") {
     cipher = createCipheriv(algorithm, key, null); // No IV for RC4
     encrypted = cipher.update(input, "utf8", "hex");
     encrypted += cipher.final("hex");
-    return encrypted;
   } else {
     cipher = createCipheriv(algorithm, key, null); // No IV for RC4
     encrypted = cipher.update(input);
     encrypted = Buffer.concat([encrypted, cipher.final()]);
-    return encrypted;
   }
-}
-
-// Decrypt using RC4, similar to AES
-export function decryptRC4(encryptedInput: Buffer | string): Buffer | string {
-  let decipher, decrypted;
-  const encryptionKeyHex = "3f27cb95e2d8d29600ca4e860a1f3ca3";
-  const key = Buffer.from(encryptionKeyHex, "hex");
-  console.log("ini key: ", key);
-  if (typeof encryptedInput === "string") {
-    decipher = createDecipheriv("rc4", key, ""); // No IV for RC4
-    decrypted = decipher.update(encryptedInput, "hex", "utf8");
-    decrypted += decipher.final("utf8");
-    return decrypted;
-  } else {
-    decipher = createDecipheriv(algorithm, key, null); // No IV for RC4
-    decrypted = decipher.update(encryptedInput);
-    decrypted = Buffer.concat([decrypted, decipher.final()]);
-    return decrypted;
-  }
+  console.timeEnd("RC4 Encryption Time");
+  return encrypted;
 }
